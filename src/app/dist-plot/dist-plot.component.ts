829e061fb49dce023bfb7e8cfd5a3f71dfb1ea31
ongdk1997 <47091141+ongdk1997@users.noreply.github.com>
2019-08-15 14:55:20 +0100

    Distribution curve added

--- src/app/dist-plot/dist-plot.component.ts
+++ src/app/dist-plot/dist-plot.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-dist-plot',
+  selector: 'dist-plot',
   templateUrl: './dist-plot.component.html',
   styleUrls: ['./dist-plot.component.css']
 })
 export class DistPlotComponent implements OnInit {
+  @Input() percentiles:number[];
+  series: number[][]=[];
 
   constructor() { }
 
   ngOnInit() {
+  	this.append();
+  }
+
+  append(){
+  	var diff = (this.percentiles[10]-this.percentiles[0])*0.1;
+  	for (var i = 0; i<10; i++){
+  		var lower = this.percentiles[i];
+  		var higher = this.percentiles[i+1];
+  		this.series.push([(higher+lower)*0.5,diff/(higher-lower)]);
+  	}
+    console.log(this.series);
   }
 
 }
